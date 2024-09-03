cd5bae0368ec6e05b535c06e2c78753e0dc47b67
seokwoan <suporterseo@gmail.com>
2024-09-03 15:23:59 +0900

    Initial commit

---
+++ src/main/java/com/project1st/Control/TestControl.java
@@ -0,0 +1,13 @@
+package com.project1st.Control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestControl {
+
+  @GetMapping( "/" )
+  public String home(){
+    return "test1";
+  }
+}
---
+++ src/test/java/com/project1st/Project1stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
