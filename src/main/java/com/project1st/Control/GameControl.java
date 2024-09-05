766fe1326eecf82d7a9c65ba5d10220ec54dca92
seokwoan <suporterseo@gmail.com>
2024-09-05 14:26:12 +0900

    Entity 3일차 점심

--- src/main/java/com/project1st/Control/GameControl.java
+++ src/main/java/com/project1st/Control/GameControl.java
@@ -11,10 +11,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping( "/game" )
 public class GameControl {
 
+  // Omok 맵핑
   @GetMapping( "/omok" )
   public String omok(){
     return "game/omok";
   }
+
+  // Hit 맵핑
+  @GetMapping( "/Hit" )
+  public String Hit(){
+    return "game/Hit";
+  }
+
+
+  // Blackjack 맵핑
+  @GetMapping( "/BlackJack" )
+  public String BlackJack(){
+    return "game/BlackJack";
+  }
+
+
+  // Number 맵핑
+  @GetMapping( "/Number" )
+  public String Number(){
+    return "game/Number";
+  }
+
 
 
 }
